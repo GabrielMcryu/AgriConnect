0f01008570d55d9c2956f3b07f9c86902c1d333f
GabrielMcryu <gabrielmcryu@gmail.com>
2023-06-13 15:29:08 +0300

    AGC-1 Created .net project and models

---
+++ agri-connect-backend/agri-connect-backend/Models/SupplierTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace agri_connect_backend.Models
+{
+    public class SupplierTransaction
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public int Cost { get; set; }
+    }
+}
