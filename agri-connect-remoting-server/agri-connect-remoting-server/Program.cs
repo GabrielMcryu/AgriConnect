4ac41096cc8750201081881d63cf1b4609892b5a
GabrielMcryu <gabrielmcryu@gmail.com>
2023-06-21 08:50:52 +0300

    AGC-6 Created .net remoting server project

---
+++ agri-connect-remoting-server/agri-connect-remoting-server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agri_connect_remoting_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
