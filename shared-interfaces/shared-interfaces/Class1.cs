ca64e9dbf366fd795e36af55500e132880dd0244
GabrielMcryu <gabrielmcryu@gmail.com>
2023-06-21 10:53:15 +0300

    AGC-6 Implemented library to connect remoting client and server

---
+++ shared-interfaces/shared-interfaces/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared_interfaces
+{
+    public interface ISupplierProduct
+    {
+    }
+
+    public interface ISupplierTransaction
+    {
+
+    }
+
+    public interface IVendorProduct
+    {
+
+    }
+
+    public interface IVendorTransaction
+    {
+
+    }
+}
