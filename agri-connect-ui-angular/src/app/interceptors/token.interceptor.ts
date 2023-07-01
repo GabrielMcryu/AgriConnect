00d8bd7baf8b45c25edf45ce84ead8638b3b466d
GabrielMcryu <gabrielmcryu@gmail.com>
2023-07-01 16:10:11 +0300

    AGC-11 Set up Services to consume authentication endpoints

---
+++ agri-connect-ui-angular/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { AuthService } from '../services/authentication/auth.service';
+import { Router } from '@angular/router';
+import { TokenApiModel } from '../models/token-api.model';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const myToken = this.auth.getToken();
+
+    if (myToken) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${myToken}` },
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError((err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            return this.handleUnAuthorizedError(request, next);
+          }
+        }
+        return throwError(() => new Error('Some other error occured'));
+      })
+    );
+  }
+
+  handleUnAuthorizedError(req: HttpRequest<any>, next: HttpHandler) {
+    let tokenApiModel = new TokenApiModel();
+    tokenApiModel.accessToken = this.auth.getToken()!;
+    tokenApiModel.refreshToken = this.auth.getRefreshToken()!;
+    console.log(tokenApiModel.accessToken);
+    return this.auth.renewToken(tokenApiModel).pipe(
+      switchMap((data: TokenApiModel) => {
+        this.auth.storeRefreshToken(data.refreshToken);
+        this.auth.storeToken(data.accessToken);
+        req = req.clone({
+          setHeaders: { Authorization: `Bearer ${data.accessToken}` },
+        });
+        return next.handle(req);
+      }),
+      catchError((err) => {
+        return throwError(() => {
+          alert('Token has expired, Please Login again');
+          this.router.navigate(['login']);
+        });
+      })
+    );
+  }
+}
